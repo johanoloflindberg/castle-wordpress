ca2c150cc4dc3ee74af0cfeedd205bd5805c64a7
Sebastian Wallin <sebastian.wallin@gmail.com>
2014-03-22 22:04:07 -0700

    Initial commit

---
+++ views/settings.php
@@ -0,0 +1,58 @@
+<div class="wrap">
+  <form action="options.php" method="post">
+    <?php screen_icon(); ?>
+    <h2><?php _e('Userbin'); ?></h2>
+<!--
+    <p>
+      <?php _e('This is where you enter you Userbin credentials and make various settings'); ?>
+    </p>
+
+    <h3><?php _e('Generic Settings'); ?></h3>
+//-->
+
+    <table class="form-table">
+      <tbody>
+        <tr valign="top">
+          <th scope="row">
+            <label for="<?php self::_settings_id('appId'); ?>"><?php _e('Application ID'); ?></label>
+          </th>
+          <td>
+            <input type="number"
+              class="regular-text"
+              id="<?php self::_settings_id('appId'); ?>"
+              name="<?php self::_settings_name('appId'); ?>"
+              value="<?php esc_attr_e($settings['appId']); ?>" />
+            <p class="description"><?php _e('Userbin application ID'); ?></p>
+          </td>
+        </tr>
+
+        <tr valign="top">
+          <th scope="row">
+            <label for="<?php self::_settings_id('apiSecret'); ?>"><?php _e('API Secret'); ?></label>
+          </th>
+          <td>
+            <input type="text"
+              class="regular-text code"
+              id="<?php self::_settings_id('apiSecret'); ?>"
+              name="<?php self::_settings_name('apiSecret'); ?>"
+              value="<?php esc_attr_e($settings['apiSecret']); ?>" />
+          </td>
+        </tr>
+
+        <tr valign="top">
+          <th scope="row">
+            <label>Userbin active</label>
+          </th>
+          <td>
+            <?= $settings['activated'] ? 'yes' : 'no' ?>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <p class="submit">
+      <?php settings_fields(self::SETTINGS_NAME); ?>
+      <input type="submit" class="button button-primary" value="<?php _e('Save Changes'); ?>" />
+    </p>
+  </form>
+</div>
